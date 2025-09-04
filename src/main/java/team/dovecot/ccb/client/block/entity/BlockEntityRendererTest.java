4c5560f4f36761596775180d3c2dbba2100a4a69
arrokoth <arrokoth233@gmail.com>
2025-09-04 20:05:58 +0200

    baka

--- src/main/java/team/dovecot/ccb/client/block/entity/BlockEntityRendererTest.java
+++ src/main/java/team/dovecot/ccb/client/block/entity/BlockEntityRendererTest.java
@@ -35,7 +35,6 @@ public class BlockEntityRendererTest implements BlockEntityRenderer<BlockEntityT
 //        builder.vertex(1, 1, 0).color(0f, 0f, 1f, 1f).endVertex();
         RenderSystem.setShader(GameRenderer::getPositionColorShader);
         tesselator.end();
-        DefaultVertexFormat
         poseStack.popPose();
     }
 
