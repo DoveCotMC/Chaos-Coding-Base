9a147d2cc54a042d44592376b3d6bcc5d18e2a8b
arrokoth <arrokoth233@gmail.com>
2025-09-23 12:29:15 +0200

    dynamic light

--- src/main/java/team/dovecot/ccb/client/block/entity/BlockEntityRendererTest.java
+++ src/main/java/team/dovecot/ccb/client/block/entity/BlockEntityRendererTest.java
@@ -1,63 +1,27 @@
 package team.dovecot.ccb.client.block.entity;
 
-import com.mojang.blaze3d.platform.NativeImage;
-import com.mojang.blaze3d.platform.TextureUtil;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
-import net.minecraft.client.renderer.texture.SimpleTexture;
-import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 import org.lwjgl.opengl.GL11;
 import team.dovecot.ccb.client.ChaosBaseClient;
 import team.dovecot.ccb.client.renderer.IRenderContext;
-import team.dovecot.ccb.client.renderer.Renderer;
 import team.dovecot.ccb.client.renderer.WrappedVertexBuffer;
-import team.dovecot.ccb.client.renderer.model.ResourceIdentifier;
-import team.dovecot.ccb.client.renderer.model.UploadedModel;
-import team.dovecot.ccb.common.ChaosBase;
 import team.dovecot.ccb.common.block.entity.BlockEntityTest;
 
 public class BlockEntityRendererTest implements BlockEntityRenderer<BlockEntityTest> {
     @Override
     public void render(BlockEntityTest blockEntity, float f, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j) {
-//        Tesselator tesselator = Tesselator.getInstance();
-
         RenderSystem.enableDepthTest();
         RenderSystem.depthMask(true);
         RenderSystem.depthFunc(GL11.GL_LEQUAL);
         RenderSystem.enableCull();
 
-        // TODO: I need a new model format ToT
-//        UploadedModel model = Renderer.getInstance().getModel(new ResourceIdentifier(ChaosBase.MOD_ID, "utah_teapot"));
-//        if (model != null) {
-//            model.drawWithShader();
-//        }
-
-//        BufferBuilder builder = tesselator.getBuilder();
-
         poseStack.pushPose();
-//        builder.begin(VertexFormat.Mode.TRIANGLES, DefaultVertexFormat.POSITION_COLOR_TEX);
-//        builder.vertex(poseStack.last().pose(), 0, 0, 0).color(1f, 1f, 1f, 1f).uv(0f, 0f).endVertex();
-//        builder.vertex(poseStack.last().pose(), 1, 0, 0).color(1f, 1f, 1f, 1f).uv(1f, 0f).endVertex();
-//        builder.vertex(poseStack.last().pose(), 0.5f, 1, 0).color(1f, 1f, 1f, 1f).uv(0.5f, 1f).endVertex();
-////        builder.vertex(0, 0, 0).color(1f, 0f, 0f, 1f).endVertex();
-////        builder.vertex(1, 0, 0).color(0f, 1f, 0f, 1f).endVertex();
-////        builder.vertex(1, 1, 0).color(0f, 0f, 1f, 1f).endVertex();
-//        RenderSystem.setShaderTexture(0, ChaosBaseClient.testModel.children.get("cylinder").texture.values().stream().toList().get(0));
-//        RenderSystem.setShader(GameRenderer::getPositionColorTexShader);
-//        tesselator.end();
         WrappedVertexBuffer buffer = WrappedVertexBuffer.upload(ChaosBaseClient.testModel, poseStack.last().pose(), poseStack.last().normal(), i, j);
-//        RenderSystem.setShader(GameRenderer::getPositionColorTexShader);
         buffer.renderAll(new IRenderContext() {
             @Override
             public Matrix4f getPoseMatrix() {
